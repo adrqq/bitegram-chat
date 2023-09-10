a160cc8460186917850355e43118dafcdeb7cfa2
Sasha-Maniak <116157371+metalikaqq@users.noreply.github.com>
2023-09-10 20:51:16 +0300

    add chat header

---
+++ src/components/OpenChatView/ChatFooter/ChatFooter.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import s from "./ChatFooter.module.scss";
+
+export const ChatFooter = () => {
+  return (
+    <div className={s.chat_footer}>
+      App
+    </div>
+  )
+}
---
+++ src/components/OpenChatView/ChatMain/ChatMain.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import s from "./ChatMain.module.scss";
+
+export const ChatMain = () => {
+  return (
+    <div className={s.chat_main}>
+      App
+    </div>
+  )
+}
+
